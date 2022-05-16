8d59ad688fa39a439ad821cb546985b015b47fbd
Ben <happy88717@gmail.com>
2022-05-16 23:12:33 +0800

    describe expect price count

--- apps/potter/src/app/optimalbuy.spec.ts
+++ apps/potter/src/app/optimalbuy.spec.ts
@@ -11,58 +11,58 @@ describe('OptimalBuy', () => {
     order = new OptimalBuy();
   });
   
-  test('0-0-0-0-0',() => {
+  test('0-0-0-0-0',() => {          //0 * 8
     order.bookorder_list([]);
     expect(order.price).toBe(0);
   });
 
-  test('1-0-0-0-0',() => {
+  test('1-0-0-0-0',() => {          //1 * 8
     order.bookorder_list([0]);
     expect(order.price).toBe(8);
   });
-  test('0-1-0-0-0',() => {
+  test('0-1-0-0-0',() => {          //1 * 8
     order.bookorder_list([1]);
     expect(order.price).toBe(8);
   });
-  test('0-0-1-0-0',() => {
+  test('0-0-1-0-0',() => {          //1 * 8
     order.bookorder_list([2]);
     expect(order.price).toBe(8);
   });
-  test('0-0-0-1-0',() => {
+  test('0-0-0-1-0',() => {          //1 * 8
     order.bookorder_list([3]);
     expect(order.price).toBe(8);
   });
 
-  test('0-0-0-0-1',() => {
+  test('0-0-0-0-1',() => {          //1 * 8
     order.bookorder_list([4]);
     expect(order.price).toBe(8);
   });
 
-  test('2-0-0-0-0',() => {
+  test('2-0-0-0-0',() => {          //2 * 8
     order.bookorder_list([0,0]);
     expect(order.price).toBe(16);
   });
-  test('2-1-0-0-0',() => {
+  test('2-1-0-0-0',() => {          //2 * 8 * 0.95 + 1 * 0.8
     order.bookorder_list([0,0,1]);
     expect(order.price).toBe(23.2);
   });
 
-  test('2-1-1-0-0',() => {
+  test('2-1-1-0-0',() => {           //3 * 8 * 0.9 + 1 * 8 * 0.8
     order.bookorder_list([0,0,1,2]);
     expect(order.price).toBe(29.6);
   });
 
-  test('1-1-0-0-0',() => {
+  test('1-1-0-0-0',() => {            //2 * 8 * 0.95
     order.bookorder_list([0,1]);
     expect(order.price).toBe(15.2);
   });
 
-  test('1-1-1-0-0',() => {
+  test('1-1-1-0-0',() => {            //3 * 8 * 0.9
     order.bookorder_list([0,1,2]);
     expect(order.price).toBe(21.6);
   });
 
-  test('1-1-1-1-0',() => {
+  test('1-1-1-1-0',() => {            //4 * 8 * 0.8
     order.bookorder_list([0,1,2,3]);
     expect(order.price).toBe(25.6);
   });
